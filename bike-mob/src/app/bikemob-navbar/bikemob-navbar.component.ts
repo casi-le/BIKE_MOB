6cec6918bf057c5f4a8f5ca4bad4e11eb4505695
casi-le <casi.popko@gmail.com>
2021-10-08 19:47:04 -0600

    Footer/header ux updates

--- bike-mob/src/app/bikemob-navbar/bikemob-navbar.component.ts
+++ bike-mob/src/app/bikemob-navbar/bikemob-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bikemob-navbar',
@@ -6,11 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bikemob-navbar.component.css']
 })
 export class BikemobNavbarComponent implements OnInit {
-
-  constructor() {   }
+  navbar: any;
+  sticky: any;
+  constructor() { }
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event: any){
+    // console.log($event);
+    // console.log("scrolling");
+    console.log(this.navbar);
+    if(window.pageYOffset>=this.sticky){
+      this.navbar?.classList.add('sticky');
+    }else{
+      this.navbar?.classList.remove('sticky');
+    }
+  } 
 
   ngOnInit(): void {
-
+    this.navbar =  document.getElementById("navbar");
+    if(this.navbar){
+      this.sticky= this.navbar.offsetTop +20;
+    }
   }
+
 
 }
