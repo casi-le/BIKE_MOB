6b19823ebe35153c0da58510a191ee9792a158ee
casi-le <casi.popko@gmail.com>
2021-11-16 14:36:29 -0700

    Lots of css updates

--- bike-mob/src/assets/script/icon-toggle.js
+++ bike-mob/src/assets/script/icon-toggle.js
@@ -8,8 +8,16 @@ function iconToggle(element){
     console.log(document);
     var allIcons = document.getElementsByTagName("i");
     for(var i=0; i<allIcons.length; i++){
-        allIcons[i].classList.remove('fa-minus-circle');
+        if(allIcons[i].classList.contains('iconChange')){
+            allIcons[i].classList.remove('fa-minus-circle');
+            allIcons[i].classList.add('fa-plus-circle');
+        }
+        
     }
     var icon = element.getElementsByTagName("i")[0];
-    icon.classList.toggle('fa-minus-circle');
+    if(icon.classList.contains('iconChange')){
+        icon.classList.toggle('fa-minus-circle');
+        icon.classList.toggle('fa-plus-circle');
+    }
+
   }
