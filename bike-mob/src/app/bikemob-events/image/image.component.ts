e0a687f4314fc6891e70bc0b76c937916bd67630
casi-le <casi.popko@gmail.com>
2021-11-17 15:30:34 -0700

    image /budget update

---
+++ bike-mob/src/app/bikemob-events/image/image.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import 
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.css']
+})
+export class ImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
