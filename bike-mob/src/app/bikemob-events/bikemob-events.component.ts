08a9524982e26ae6406aef28d1ff9a78f1b4d14b
casi-le <casi.popko@gmail.com>
2021-11-24 14:05:02 -0700

    mobile event adjust

--- bike-mob/src/app/bikemob-events/bikemob-events.component.ts
+++ bike-mob/src/app/bikemob-events/bikemob-events.component.ts
@@ -17,11 +17,19 @@ export class BikemobEventsComponent implements OnInit {
     this.calendarEl = document.getElementById('calendar');
     let calendar = new Calendar(this.calendarEl, {
       initialView: 'dayGridMonth',
+      customButtons: {
+        custom: {
+          text: 'Calendar',
+          click: function() {
+            window.open('https://calendar.google.com/calendar/embed?src=focobikemob%40gmail.com&ctz=America%2FDenver', '_blank')?.focus()
+          }
+        }
+      },
       plugins: [ googleCalendarPlugin ],
       headerToolbar: {
-        left: 'dayGridMonth,timeGridWeek',
+        left: 'dayGridMonth,timeGridWeek,custom',
         center: 'title',
-        right: 'prev,today,next'
+        right: 'prev,next'
       }, 
       titleFormat: { year: 'numeric', month: 'short' },
       googleCalendarApiKey: 'AIzaSyC2ZSubbhIc3zuaJl9qtG9IVQajGptb66A',
@@ -31,13 +39,27 @@ export class BikemobEventsComponent implements OnInit {
       eventColor: '#FF69B4',
       eventDisplay: 'block',
       eventClick: function(info){
-        info.jsEvent.preventDefault(); // don't let the browser navigate
-        if (info.event.url) {
-          window.open(info.event.url, '_blank').focus();
+        info.jsEvent.preventDefault(); // don't let the browser navigate 
+        var eventUrl = info.event.url;
+        const ua = navigator.userAgent;
+        var mobile = false;
+        if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)){
+          mobile = true;
         }
+       if(mobile==true){
+        if (eventUrl) {
+          var index = eventUrl.indexOf('event');
+          var endindex = 'event?eid='.length;
+          var url = eventUrl.slice(0,index) + 'r/eventedit/copy/' + eventUrl.slice(index+ endindex, eventUrl.length)
+          window.open(url, '_blank')?.focus();
+        }
+       }else{
+         if(eventUrl){
+          window.open(eventUrl, '_blank');
+         }
+       }     
       }
     });
     calendar.render();
-    
   }
 }
