b55d3e95a35c251003e58e8bbf710552a79ecf04
casi-le <casi.popko@gmail.com>
2022-02-04 15:56:45 -0700

    Updates to core principles home page

--- bike-mob/src/app/bikemob-home/bikemob-home.component.ts
+++ bike-mob/src/app/bikemob-home/bikemob-home.component.ts
@@ -12,8 +12,8 @@ export class BikemobHomeComponent implements OnInit {
   hoverP:any;
 
   ngOnInit(): void {
-    this.hoverEl = document.getElementById('radicalH');
-    this.hoverP=document.getElementById('radicalP')
+    this.hoverEl = document.getElementById('unhingedH');
+    this.hoverP=document.getElementById('unhingedP')
   }
   applyHover(el: any, elP: any){
     if(this.hoverEl){
